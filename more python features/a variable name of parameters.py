148bae78e889cad6b7df8e48f257195a7df07f19
Asadbek <k@Kui-MacBookAir.local>
2024-01-13 07:43:00 +0900

    some more python features

---
+++ more python features/a variable name of parameters.py
@@ -0,0 +1,4 @@
+def testing(*my_args):
+    print(len(my_args))
+    print(sum(my_args))
+testing(2,3,4,5,6,7)
---
+++ more python features/default parameter value.py
@@ -0,0 +1,4 @@
+def say_hello(name= "Emily"):
+    print("Hello",name)
+say_hello()
+say_hello("")
