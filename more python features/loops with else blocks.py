24414e8eca35b858eb0e67026c151b558489b42d
Asadbek <k@Kui-MacBookAir.local>
2024-01-13 07:30:18 +0900

    loops with else blocks are added

---
+++ more python features/loops with else blocks.py
@@ -0,0 +1,9 @@
+my_list = [3,2,1,5,8,9]
+for number in my_list:
+    if number % 2 == 0:
+        print("Found an even number",number)
+        break
+    else:
+        print("no even number in the list")
+
+
---
+++ more python features/ternary operator.py
@@ -0,0 +1,2 @@
+x = int(input("Enter a value: "))
+print("even" if x % 2 ==0 else "odd")
